274c5146b511d2d7b715c27d392c4b5d97394bc7
Evandro Rodrigues de Arruda <161489446+HubDoVandinho@users.noreply.github.com>
2025-08-27 17:20:13 -0300

    Add files via upload

---
+++ prjExemplo1/src/Exemplo1.java
@@ -0,0 +1,29 @@
+
+
+/**
+ * @author Vandinho
+ */
+public class Exemplo1 {
+
+
+    public static void main(String[] args) {
+        int idade;
+        double peso;
+        char sexo;
+        String nome;
+        
+        idade = 20;
+        peso = 81.5;
+        sexo = 'M';
+        nome = "Vandinho";
+        
+        System.out.println("Seu nome é: " + nome);
+        System.out.println("Sua idade é: " + idade);
+        System.out.println("seu genero é: " + sexo);
+        System.out.println("Seu peso é de: " + peso + "Kg");
+        
+        
+        
+    }
+    
+}
