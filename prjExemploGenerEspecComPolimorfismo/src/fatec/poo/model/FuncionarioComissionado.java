9a51c5d5a7d8724e98dc488311cb2dd32606ce63
Evandro Rodrigues de Arruda <161489446+HubDoVandinho@users.noreply.github.com>
2025-09-17 10:54:14 -0300

    Add files via upload

---
+++ prjExemploGenerEspecComPolimorfismo/src/fatec/poo/model/FuncionarioComissionado.java
@@ -0,0 +1,56 @@
+package fatec.poo.model;
+
+/**
+ * @author Vandinho
+ */
+public class FuncionarioComissionado extends Funcionario {
+
+    private double salBase;
+    private double taxaComissao;
+    private double totalVendas;
+
+    public FuncionarioComissionado(int r, String n, String dtAdm, double tC) {
+        super(r, n, dtAdm);
+        taxaComissao = tC;
+    }
+
+    public void setSalBase(double sB) {
+        salBase = sB;
+    }
+
+    public double getSalbase() {
+        return salBase;
+    }
+
+    public double getTotalVendas() {
+        return totalVendas;
+    }
+
+    public double getTaxaComissao() {
+        return taxaComissao;
+    }
+
+    public void addVendas(Double v) {
+        totalVendas += v;
+    }
+
+    public double calcSalBruto() {
+        return (salBase + taxaComissao * totalVendas);
+    }
+
+    public double calcGratificacao() {
+        if (totalVendas > 10000) {
+            return (calcSalBruto() * 0.05);
+        } else {
+            if (totalVendas > 5000) {
+                return (calcSalBruto() * 0.03);
+            } else {
+                return 0;
+            }
+        }
+    }
+    
+    public double calcSalLiquido(){
+        return (calcSalBruto() - calcDesconto() + calcGratificacao());
+    }
+}
